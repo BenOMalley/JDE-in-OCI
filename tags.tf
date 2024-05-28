2d5590dcdce8eb27a88af0a39904de714408446b
Ben-OMalley <ben_omalley@outlook.com>
2024-05-29 08:24:08 +1000

    Final testing of comment

--- tags.tf
+++ tags.tf
@@ -16,7 +16,7 @@ resource "oci_identity_tag" "tag1" {
 }
 
 resource "oci_identity_tag" "tag2" {
-  #Required
+  #Required.
   description      = "tf example tag 2"
   name             = "tf-example-tag-2"
   tag_namespace_id = oci_identity_tag_namespace.tag-namespace1.id
